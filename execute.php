c5da9b273f392c3d92d66368a914db5b2e5225d5
Gabriele Tavella <gabtavella@gmail.com>
2017-06-22 21:25:35 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -19,16 +19,9 @@ if($text == "/start"){
 
 
 
+/*
 
-
-
-if(strpos($text, "/start") == "/start")
-{
-	// imposto la keyboard
-	$parameters["reply_markup"] = '{ "keyboard": [[], [""], [""], [""]], "one_time_keyboard": false}';
-	$response = "Benvenuto in AlterBot \nIl bot di Alter.Polis per aiutare gli studenti \nCosa vuoi fare?";
-}
-elseif($text == "🎓 Chi siamo")
+if($text == "🎓 Chi siamo")
 {
 	$parameters["reply_markup"] = '{ "keyboard": [["Alter.Polis"], ["LINK"], ["Indietro"]], "one_time_keyboard": false, "resize_keyboard": true}';
 }
@@ -41,19 +34,5 @@ elseif($text == "🌎 Lingua")
 	$parameters["reply_markup"] = '{ "keyboard": [["Italiano"], ["English"], ["Français"], ["中文"] ["Indietro"]], "one_time_keyboard": true, "resize_keyboard": true}';
 	$response = "Feature in allestimento";
 }
-elseif($text == "❤ Credits")
-{
-	$response = "";
-}
-else
-{
-	$response = $text;  //debug only
-}
 
-header("Content-Type: application/json");
-// rispondo con array JSON composto da chat_id, text, method
-$parameters['chat_id'] = $chatId;
-
-$parameters["text"] = $response;
-$parameters["method"] = "sendMessage";
-echo json_encode($parameters);
+*/
