37b46a7be2a84d56922a20eb27aab3e90c6c68da
Gabriele Tavella <gabtavella@gmail.com>
2017-06-22 23:41:38 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -18,9 +18,9 @@ if(callback($update)){
 		editmsg($chatId, $msgid, $button, $words);		
 	}
 	elseif($cbdata == "menu_1"){
-		$button[] = array(array("text" => "Alter.Polis", "callback_data" => "menu_1"), array("text" => "LINK", "callback_data" => "menu_2"),);
-		$button[] = array(array("text" => "Indietro", "callback_data" => "menu_0"),);
-		editmsg($chatId, $msgid, $button, "lorem ipsum");
+		//$button[] = array(array("text" => "Alter.Polis", "callback_data" => "menu_1"), array("text" => "LINK", "callback_data" => "menu_2"),);
+		//$button[] = array(array("text" => "Indietro", "callback_data" => "menu_0"),);
+		//editmsg($chatId, $msgid, $button, "lorem ipsum");
 		sendMess($cbid, "ritenta");
 	}
 	elseif($cbdata == "menu_2"){
