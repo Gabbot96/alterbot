80d742284003daa4ec569237f4ca28196dfd40b3
Gabriele Tavella <gabtavella@gmail.com>
2017-06-23 09:57:00 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -7,38 +7,47 @@ include 'src.php';
 $words = "Benvenuto in AlterBot \nIl bot di Alter.Polis per aiutare gli studenti \nCosa vuoi fare?";
 
 if($text == "/start"){
-	$button[] = array(array("text" => "🎓 Chi siamo", "callback_data" => "menu_1"), array("text" => "✉ Contattaci", "callback_data" => "menu_2"),);
+	$button[] = array(array("text" => "🎓 Chi siamo", "callback_data" => "menu_1"), array("text" => "✉ Rappresentanti", "callback_data" => "menu_2"),);
 	$button[] = array(array("text" => "🌎 Lingua", "callback_data" => "menu_3"), array("text" => "❤ Credits", "callback_data" => "menu_4"),);
 	inlinekeyboard($button, $chatId, $words);	
 }
 
 if(callback($update)){
 	if($cbdata == "menu_0"){ //menu principale
-		$button[] = array(array("text" => "🎓 Chi siamo", "callback_data" => "menu_1"), array("text" => "✉ Contattaci", "callback_data" => "menu_2"),);
+		$button[] = array(array("text" => "🎓 Chi siamo", "callback_data" => "menu_1"), array("text" => "✉ Rappresentanti", "callback_data" => "menu_2"),);
 		$button[] = array(array("text" => "🌎 Lingua", "callback_data" => "menu_3"), array("text" => "❤ Credits", "callback_data" => "menu_4"),);
 		editmsg($chatId, $msgid, $button, $words);		
 	}
-	elseif($cbdata == "menu_1"){
-		$button[] = array(array("text" => "Alter.Polis", "callback_data" => "menu_1a"), array("text" => "LINK", "callback_data" => "menu_1b"),);
+	elseif($cbdata == "menu_1"){ //Chi siamo
+		$button[] = array(array("text" => "Alter.Polis", "url" => "alterpolis.it/it/homepage/"), array("text" => "LINK", "url" => "linkcoordinamentouniversitario.it/"),);
 		$button[] = array(array("text" => "Indietro", "callback_data" => "menu_0"),);
-		editmsg($cbid, $msgid, $button, "questo è il menu 1");
+		editmsg($cbid, $msgid, $button, "questo è il menu 1"); //inserire descrizione del collettivo e di LINK
 
 	}
-	elseif($cbdata == "menu_2"){
+	elseif($cbdata == "menu_2"){ // Rappresentanti
 		$button[] = array(array("text" => "Organi Centrali", "callback_data" => "menu_2a"), array("text" => "Organi Periferici", "callback_data" => "menu_2b"),);
 		$button[] = array(array("text" => "Indietro", "callback_data" => "menu_0"),);
-		editmsg($cbid, $msgid, $button, "questo è il menu 2");		
+		editmsg($cbid, $msgid, $button, "questo è il menu 2");	//due parole sui nostri numeri negli organi	
 	}
-	elseif($cbdata == "menu_3"){
+	elseif($cbdata == "menu_3"){ //Lingua
 		$button[] = array(array("text" => "Italiano", "callback_data" => "it"), array("text" => "English", "callback_data" => "en"),);
 		$button[] = array(array("text" => "Italiano", "callback_data" => "fr"), array("text" => "中文", "callback_data" => "cc"),);
 		$button[] = array(array("text" => "Indietro", "callback_data" => "menu_0"),);
-		inlinekeyboard($button, $chatId, "");		
+		editmsg($cbid, $msgid, $button, "Questa feature sarà presto disponibile\n\nThis feature will be available soon");  
+		
 	}
-	elseif($cbdata == "menu_4"){
-		$button[] = array(array("text" => "Alter.Polis", "callback_data" => "menu_1"), array("text" => "LINK", "callback_data" => "menu_2"),);
+	elseif($cbdata == "menu_4"){ //Credits
 		$button[] = array(array("text" => "Indietro", "callback_data" => "menu_0"),);
-		inlinekeyboard($button, $chatId, "");		
+		editmsg($cbid, $msgid, $button, "Bot sviluppato da: Gabriele Tavella\nCon l'aiuto di:\n David Chiappini\nMarco Rondina");
+		
+	}
+	elseif($cbdata == "menu_2a"){ // Organi Centrali
+		$button[] = array(array("text" => "Indietro", "callback_data" => "menu_2"),);
+		editmsg($cbid, $msgid, $button, "questo è il menu 2a");		
+	}
+	elseif($cbdata == "menu_2a"){ // Organi Periferici
+		$button[] = array(array("text" => "Indietro", "callback_data" => "menu_2"),);
+		editmsg($cbid, $msgid, $button, "questo è il menu 2b");	//implementare qua la ricerca	
 	}
 }
 
