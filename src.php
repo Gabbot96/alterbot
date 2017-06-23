bfa6da04bf6a5b9c24d7f34ddbd0bcd64da4e74f
Gabriele Tavella <gabtavella@gmail.com>
2017-06-23 11:14:05 +0200

    Added comments
    I'm stupid and I can't code like it should be done (cit. me)

--- src.php
+++ src.php
@@ -5,6 +5,7 @@ define('api', 'https://api.telegram.org/bot'.token.'/');
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 
+//data fields del messaggio
 $message = isset($update['message']) ? $update['message'] : "";
 $messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
@@ -14,6 +15,7 @@ $username = isset($message['chat']['username']) ? $message['chat']['username'] :
 $date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
 
+//data fields delle query
 $cbid = $update['callback_query']['from']['id'];
 $cbdata = $update['callback_query']['data'];
 $msgid = $update['callback_query']['message']['message_id'];
@@ -24,11 +26,13 @@ function callback($up){
 	return $up["callback_query"]; 
 }
 
+//funzione che esegue la richiesta web tramite URL
 function request($method){
         $req = file_get_contents(api.$method);
         return $req;     
 }
 
+//funzione per l'utilizzo del metodo sendMessage
 function sendMess($id, $urltext){
         if(strpos($urltext, "\n")){
 		$urltext = urlencode($urltext);
@@ -36,6 +40,7 @@ function sendMess($id, $urltext){
 	return request("sendMessage?text=$urltext&parse_mode=HTML&chat_id=$id");
 }
 
+//funzione per l'encoding di tastiere inline 
 function inlinekeyboard($layout, $id, $msgtext){
         if(strpos($msgtext, "\n")){
 		$msgtext = urlencode($msgtext);
@@ -45,6 +50,7 @@ function inlinekeyboard($layout, $id, $msgtext){
 	return request("sendMessage?text=$msgtext&parse_mode=Markdown&chat_id=$id&reply_markup=$keyboard");
 }
 
+//funzione per l'update dei messaggi e l'update di tastiere inline 
 function editmsg($chat_id, $msg, $inline, $edited){
         if(strpos($edited, "\n")){
 		$edited = urlencode($edited);
